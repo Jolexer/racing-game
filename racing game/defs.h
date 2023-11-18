2852356be5376298073b1c8a637f7bc2724e4eeb
Jolexer <jolex.nerd@gmail.com>
2023-11-18 07:40:07 +0100

    added defenitions header file

---
+++ racing game/defs.h
@@ -0,0 +1,13 @@
+//
+//  defs.h
+//  racing game
+//
+//  Created by Jorik de boer on 18/11/2023.
+//
+
+#ifndef defs_h
+#define defs_h
+
+
+
+#endif /* defs_h */
--- racing game/main.c
+++ racing game/main.c
@@ -4,7 +4,9 @@
 
 #include <stdio.h>
 
+#include "defs.h"
+
 int main(void) {
-    printf("hello world!\n");
+    
     return 0;
 }
