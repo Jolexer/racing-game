2c32f73b9aff5cde8603331d57fed559a53fc675
Jolexer <jolex.nerd@gmail.com>
2023-11-18 07:19:53 +0100

    Initial Commit

---
+++ racing game/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  racing game
+//
+//  Created by Jorik de boer on 18/11/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
